15d5bbf356a7c609bc98aadbad24986b4a82b8c4
Kevin Egger <eggerk@student.ethz.ch>
2016-09-27 19:43:30 +0200

    added i3 configuration

--- setup_links.sh
+++ setup_links.sh
@@ -6,13 +6,21 @@ CURRENT_DIR=$(pwd)
 DOTFILES_PATH="$FILEPATH$CURRENT_DIR"
 
 # zshrc
-rm "$HOME_FOLDER/.zshrc"
+rm "$HOME_FOLDER/.zshrc" > /dev/null
 ln -s "$DOTFILES_PATH/zshrc" "$HOME_FOLDER/.zshrc"
 
 # vimrc
-rm "$HOME_FOLDER/.vimrc"
+rm "$HOME_FOLDER/.vimrc" > /dev/null
 ln -s "$DOTFILES_PATH/vimrc" "$HOME_FOLDER/.vimrc"
 
 # tmux
-rm "$HOME_FOLDER/.tmux.conf"
+rm "$HOME_FOLDER/.tmux.conf" > /dev/null
 ln -s "$DOTFILES_PATH/tmux.conf" "$HOME_FOLDER/.tmux.conf"
+
+# i3
+mkdir -p "$HOME_FOLDER/.config/i3"
+mkdir -p "$HOME_FOLDER/.config/i3status"
+rm "$HOME_FOLDER/.config/i3/config" > /dev/null
+ln -s "$DOTFILES_PATH/i3/config" "$HOME_FOLDER/.config/i3/config"
+rm "$HOME_FOLDER/.config/i3status/config" > /dev/null
+ln -s "$DOTFILES_PATH/i3/statusbar" "$HOME_FOLDER/.config/i3status/config"

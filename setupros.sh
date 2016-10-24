7c75af08dadd1c7a21b410f25af78bab73c4e551
Kevin Egger <eggerk@ethz.ch>
2016-10-24 10:21:47 +0200

    added setupros script

---
+++ setupros.sh
@@ -0,0 +1,9 @@
+export CATKIN_WS=/home/eggerk/catkin_ws
+
+# ROS
+source /opt/ros/kinetic/setup.zsh
+source ~/catkin_ws/devel/setup.zsh
+alias catkin_ws='cd $CATKIN_WS'
+
+alias ckthis='catkin build --this --no-deps'
+

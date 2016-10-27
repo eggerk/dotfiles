f5be44cc832ae25234d6a0175d02c47effbb7a0e
Kevin Egger <eggerk@ethz.ch>
2016-10-27 15:29:06 +0200

    changed install script

--- setup/install_software.sh
+++ setup/install_software.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Latest i3wm
-echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
+echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get --allow-unauthenticated install sur5r-keyring
 sudo apt-get update

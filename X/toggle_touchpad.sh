c36fe2cf155013c167a19e07e50c629fd67f6440
Kevin Egger <eggerk@ethz.ch>
2016-11-30 10:49:17 +0100

    Added shortcut to toggle touchpad.

---
+++ X/toggle_touchpad.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Taken form http://askubuntu.com/a/160549
+
+declare -i ID
+ID=`xinput list | grep -Eo 'TouchPad\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
+declare -i STATE
+STATE=`xinput list-props $ID|grep 'Device Enabled'|awk '{print $4}'`
+if [ $STATE -eq 1 ]
+then
+    xinput disable $ID
+    notify-send "Touchpad disabled."
+else
+    xinput enable $ID
+    notify-send "Touchpad enabled."
+fi

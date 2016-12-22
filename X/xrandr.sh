235b9d36d6622cf460fb708c7cbb8bc9126602c1
Kevin Egger <eggerk@ethz.ch>
2016-12-22 15:55:56 +0100

    Improved monitor configuration

--- X/xrandr.sh
+++ X/xrandr.sh
@@ -12,9 +12,9 @@ function setMonitors() {
   xrandr --output $MONITOR1 --mode 1920x1080 --pos 0x0 --dpi 96
   xrandr --output $MONITOR2 --mode 1920x1200  --right-of $MONITOR1 --dpi 96
   i3-msg workspace 0
-  i3-msg move workspace to output eDP-1
+  i3-msg move workspace to output $MONITOR1
   i3-msg workspace 1
-  i3-msg move workspace to output DP-3.2.8
+  i3-msg move workspace to output $MONITOR2
   i3-msg reload
   sleep 2
   i3-msg reload

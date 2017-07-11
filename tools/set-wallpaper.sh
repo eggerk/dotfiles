b3546f383ef43d1ef82c8f04882aa0fe77f446a1
Kevin Egger <eggerk@ethz.ch>
2017-07-11 08:48:15 +0200

    Fixed wallpaper on one computer

--- tools/set-wallpaper.sh
+++ tools/set-wallpaper.sh
@@ -3,7 +3,7 @@ if echo $HOSTNAME | grep 50  ; then
   # P50
   echo 1
   nitrogen --restore &
-elif echo $HOST | grep 50 ; then
+else
   echo 2
   feh --bg-fill --randomize /home/eggerk/Pictures/wallpapers/* &
 fi

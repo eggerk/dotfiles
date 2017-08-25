3079f20eb0ed24aa3c73cefb65ada9c094836c76
Kevin Egger <eggerk@ethz.ch>
2017-08-25 12:20:32 +0200

    Slack integration

--- setup/install_software.sh
+++ setup/install_software.sh
@@ -2,26 +2,26 @@
 
 # Latest i3wm
 echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get --allow-unauthenticated install sur5r-keyring
-sudo apt-get update
+sudo apt update
+sudo apt --allow-unauthenticated install sur5r-keyring
+sudo apt update
 
 # Latest rofi
 sudo add-apt-repository ppa:aguignard/ppa
 
 # Install everything
-sudo apt-get install backintime-gnome blueman build-essential clementine \
+sudo apt install backintime-gnome blueman build-essential clementine \
     cmake colordiff default-jre gimp git gparted htop \
     inkscape network-manager-vpnc-gnome pdfshuffler \
-    playonlinux rar redshift redshift-gtk synaptic texlive-full xfce4-notifyd \
-    unrar virtualbox vlc vlc-plugin-fluidsynth vlc-plugin-notify wine1.6 \
+    rar redshift redshift-gtk synaptic texlive-full xfce4-notifyd \
+    unrar virtualbox vlc vlc-plugin-fluidsynth vlc-plugin-notify \
     xautomation xbindkeys xwiimote vim vim-gtk scrot zsh feh tmux i3 rofi
     octave
 
 # Disable nautilus Desktop rendering.
 gsettings set org.gnome.desktop.background show-desktop-icons false
 
-echo "apt-get completed ========================"
+echo "apt completed ========================"
 
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -54,6 +54,10 @@ sudo apt-get update
 
 # 4. Install Spotify
 sudo apt-get install -y spotify-client
+
+# Slack.
+wget -O "slack-desktop.deb" "https://downloads.slack-edge.com/linux_releases/slack-desktop-2.7.1-amd64.deb"
+sudo dpkg -i slack-desktop.deb
 
 # clean up
 cd ..

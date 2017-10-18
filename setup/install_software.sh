a02dc50191b51cf256447598da57417443b033f0
Kevin Egger <eggerk@ethz.ch>
2017-10-18 20:11:15 +0200

    Urxvt

--- setup/install_software.sh
+++ setup/install_software.sh
@@ -16,7 +16,7 @@ sudo apt install backintime-gnome blueman build-essential clementine \
     rar redshift redshift-gtk synaptic texlive-full xfce4-notifyd \
     unrar virtualbox vlc vlc-plugin-fluidsynth vlc-plugin-notify \
     xautomation xbindkeys xwiimote vim vim-gtk scrot zsh feh tmux i3 rofi
-    octave udiskie
+    octave udiskie rxvt-unicode-256color
 
 # Disable nautilus Desktop rendering.
 gsettings set org.gnome.desktop.background show-desktop-icons false

489323eab02755c6465733ef5eaf1e29d680aaff
Kevin Egger <eggerk@ethz.ch>
2017-11-22 14:12:12 +0100

    Switch terminal emulator again and some other things

--- setup/setup_links.sh
+++ setup/setup_links.sh
@@ -19,7 +19,6 @@ mkdir -p "$HOME_FOLDER/.config/i3"
 mkdir -p "$HOME_FOLDER/.config/i3status"
 ln -sf "$DOTFILES_PATH/i3/config" "$HOME_FOLDER/.config/i3/config"
 ln -sf "$DOTFILES_PATH/i3/statusbar" "$HOME_FOLDER/.config/i3status/config"
-ln -sf "$DOTFILES_PATH/i3/i3blocks.conf" "$HOME_FOLDER/.i3blocks.conf"
 
 # xbindkeys
 ln -sf "$DOTFILES_PATH/X/xbindkeysrc" "$HOME_FOLDER/.xbindkeysrc"

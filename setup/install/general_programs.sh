5c05174ec3f5549304f8800747456dae3739cb77
Kevin Egger <eggerk@ethz.ch>
2018-01-16 20:09:23 +0100

    Powerline fonts

--- setup/install/general_programs.sh
+++ setup/install/general_programs.sh
@@ -5,7 +5,7 @@ sudo apt install backintime-gnome blueman build-essential clementine cmake \
     colordiff default-jre gimp git gparted htop inkscape \
     network-manager-vpnc-gnome pdfshuffler rar redshift redshift-gtk synaptic \
     unrar vlc vlc-plugin-fluidsynth vlc-plugin-notify xautomation xbindkeys \
-    xwiimote zsh tmux octave udiskie fonts-powerline
+    xwiimote zsh tmux octave udiskie
 
 # Disable nautilus Desktop rendering.
 gsettings set org.gnome.desktop.background show-desktop-icons false

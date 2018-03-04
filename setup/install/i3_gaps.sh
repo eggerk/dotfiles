40cab19887efcb7a08c85647fa0fd30571960e8a
Kevin Egger <eggerk@ethz.ch>
2018-03-04 10:19:02 +0100

    i3 gaps

---
+++ setup/install/i3_gaps.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if lsb_release -c | grep xenial > /dev/null ; then
+sudo add-apt-repository ppa:aguignard/ppa
+  sudo apt-get update
+
+  sudo apt install -y libxcb-xrm-dev
+
+  sudo apt install -y \
+    libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev \
+    libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev \
+    libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+    libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf \
+    libxcb-xrm-dev
+else
+  # Different requirements for 16.10+.
+  sudo apt install -y \
+    libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev \
+    libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev \
+    libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+    libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 \
+    libxcb-xrm-dev automake
+fi
+
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+
+# compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install

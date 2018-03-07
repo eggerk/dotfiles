388c8d4b9124d5072c4400492fd62920f8ab3b7b
Kevin Egger <eggerk@ethz.ch>
2018-03-07 19:55:20 +0100

    Install some python libs

--- setup/install/general_programs.sh
+++ setup/install/general_programs.sh
@@ -5,7 +5,7 @@ sudo apt install backintime-gnome blueman build-essential clementine cmake \
     colordiff default-jre gimp git gparted htop inkscape \
     network-manager-vpnc-gnome pdfshuffler rar redshift redshift-gtk synaptic \
     unrar vlc vlc-plugin-fluidsynth vlc-plugin-notify xautomation xbindkeys \
-    xwiimote zsh tmux octave udiskie
+    xwiimote zsh tmux octave udiskie ipython python-numpy python-matplotlib
 
 # Disable nautilus Desktop rendering.
 gsettings set org.gnome.desktop.background show-desktop-icons false

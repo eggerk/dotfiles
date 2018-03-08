c0ab1c393c7b080a8e493cb22cf4398b25d7b484
Kevin Egger <eggerk@ethz.ch>
2018-03-08 08:21:05 +0100

    Need to set background multiple times

--- tools/set-wallpaper.sh
+++ tools/set-wallpaper.sh
@@ -2,4 +2,12 @@
 PICTURE_FOLDER=/home/eggerk/Dropbox/wallpapers
 echo "Running feh..."
 feh --bg-fill --randomize $PICTURE_FOLDER/* &
+if [ $1 ] ; then
+  # If given an argument it was during startup.
+  if echo $HOSTNAME | grep 50 > /dev/null || echo $HOST | grep 50 > /dev/null ; then
+    # Do it again because there still seems to be an issue somewhere.
+    sleep 0.2
+    feh --bg-fill --randomize $PICTURE_FOLDER/* &
+  fi
+fi
 echo "Done!"
--- vim/ycm/ycm_extra_conf.py
+++ vim/ycm/ycm_extra_conf.py
@@ -38,9 +38,6 @@ flags = [
 '-Wall',
 '-Wextra',
 # '-Werror',
-'-Wc++98-compat',
-'-Wno-long-long',
-'-Wno-variadic-macros',
 '-fexceptions',
 '-DNDEBUG',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which

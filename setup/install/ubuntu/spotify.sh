5c410a535f3c68bbc149b4c21307ad0d303e2b54
Kevin Egger <eggerk@ethz.ch>
2018-10-02 20:23:57 +0200

    Fix installation

--- setup/install/ubuntu/spotify.sh
+++ setup/install/ubuntu/spotify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 1. Add the Spotify repository signing keys to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 
 # 2. Add the Spotify repository
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list

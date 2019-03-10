be61cd2cbe677e8c50bc7ab079a3e12f242f771b
Kevin Egger <eggerk@ethz.ch>
2019-03-10 10:03:58 +0100

    More theme changes

--- setup/install/neovim.sh
+++ setup/install/neovim.sh
@@ -19,13 +19,17 @@ sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
 select-editor
 
+# Install vim-plug.
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Install coc.
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get -y install yarn
+
 sudo apt install nodejs rapidjson-dev zlib1g-dev ncurses-dev
 git clone --depth=1 --recursive https://github.com/MaskRay/ccls
 cd ccls
 cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release
 cmake --build Release
-
-# Install vim-plug.
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

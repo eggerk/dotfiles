98ef4cb7d8f69c1217b69842ed7e885654e9acb3
Kevin Egger <kevin.egger@sevensense.ch>
2022-07-18 07:29:42 +0200

    rustanalyzer install script

---
+++ tools/install_rust_analyzer.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mkdir -p ~/.local/bin
+rm  ~/.local/bin/rust-analyzer
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+chmod +x ~/.local/bin/rust-analyzer

3207021ae118ac2aa4c759c4c418a97169ca99f9
Kevin Egger <git@eggerk.ch>
2024-01-18 20:06:09 +0100

    Arch/endeavour setup scripts

---
+++ setup/install/arch/general_programs.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Install everything
+sudo pacman -S \
+  alacritty \
+  base-devel \
+  blueman \
+  cmake \
+  digikam \
+  exa \
+  exiv2 \
+  fd \
+  gimp \
+  git \
+  htop \
+  inkscape \
+  ipython \
+  maim \
+  ncdu \
+  neovim \
+  ranger \
+  rapid-photo-downloader \
+  redshift \
+  ripgrep \
+  rofi-calc \
+  strawberry \
+  sxiv \
+  tree \
+  udiskie \
+  xautomation \
+  xbindkeys \
+  xclip \
+  xdotool \
+  xh \
+  zsh
+
+  #backintime-qt \
+# Disable nautilus Desktop rendering.
+gsettings set org.gnome.desktop.background show-desktop-icons false

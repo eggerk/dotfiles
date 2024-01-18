bb0b2e17385283d1c794a0da5ee1621c52ceb76e
Kevin Egger <git@eggerk.ch>
2024-01-18 20:09:37 +0100

    Disable indent-blankline

--- neovim/other_plugin_config.lua
+++ neovim/other_plugin_config.lua
@@ -1,8 +1,7 @@
 require('gitsigns').setup {}
 
-require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = true,
-}
-
+-- require("indent_blankline").setup {
+--     -- for example, context is off by default, use this to turn it on
+--     show_current_context = true,
+--     show_current_context_start = true,
+-- }

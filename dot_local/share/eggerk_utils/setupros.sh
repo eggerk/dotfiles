101b82c0b39d47ca7f72db281d05fcf823f38939
Kevin Egger <kevin.egger@sevensense.ch>
2025-02-25 09:16:59 +0100

    Adapt ROS setup to ROS2

--- dot_local/share/eggerk_utils/setupros.sh
+++ dot_local/share/eggerk_utils/setupros.sh
@@ -14,69 +14,33 @@ function ckrelease() {
       -DCMAKE_CXX_FLAGS=-fdiagnostics-color
 }
 
-for folder in /home/eggerk/catkin_ws \
-              /home/eggerk/work/catkin_ws; do
+for folder in /home/eggerk/ros2_ws \
+              /home/eggerk/work/ros2_ws; do
   if [ -d "$folder"/devel ] ; then
-    export CATKIN_WS="$folder"
+    export ROS_WS="$folder"
   fi
 done
 
 # ROS
-# Assume noetic by default.
-ROS_DISTRO="noetic"
+# Assume humble by default.
+ROS_DISTRO="humble"
 
 setupros() {
   if [ "$SHELL" = "/bin/bash" ] ; then
     ROS_OPT_FILE="/opt/ros/${ROS_DISTRO}/setup.bash"
-    ROS_DEVEL_FILE="${CATKIN_WS}/devel/setup.bash"
+    ROS_DEVEL_FILE="${ROS_WS}/devel/setup.bash"
   else
     ROS_OPT_FILE="/opt/ros/${ROS_DISTRO}/setup.zsh"
-    ROS_DEVEL_FILE="${CATKIN_WS}/devel/setup.zsh"
+    ROS_DEVEL_FILE="${ROS_WS}/devel/setup.zsh"
+  fi
+  if [ -f $ROS_OPT_FILE ]; then
+    source $ROS_OPT_FILE
   fi
   if [ -f $ROS_DEVEL_FILE ]; then
       source $ROS_DEVEL_FILE
-  elif [ -f $ROS_OPT_FILE ]; then
-    source $ROS_OPT_FILE
   fi
 }
 setupros
-alias ckws='$CATKIN_WS'
-
-function cbt() {
-  if [[ -z $1 ]] ; then
-    catkin build --this --no-deps
-  else
-    catkin build $@ --no-deps
-  fi
-}
-function cbtl() {
-  if [[ -z $1 ]] ; then
-    catkin build --this --no-deps --no-status | less
-  else
-    catkin build $@ --no-deps --no-status | less
-  fi
-}
-function cb() {
-  if [[ -z $1 ]] ; then
-    catkin build --this
-  else
-    catkin build $@
-  fi
-}
-function ctt() {
-  if [[ -z $1 ]] ; then
-    catkin run_tests --this --no-deps
-  else
-    catkin run_tests $@ --no-deps
-  fi
-}
-function cttl() {
-  if [[ -z $1 ]] ; then
-    catkin run_tests --this --no-deps --no-status | less
-  else
-    catkin run_tests $@ --no-deps --no-status | less
-  fi
-}
 
 export GTEST_COLOR='yes'
 

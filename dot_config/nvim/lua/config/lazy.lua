c8786de1fed95ab39b2b1b53cd926e3c5c7d45ad
Kevin Egger <kevin.egger@sevensense.ch>
2025-06-05 13:47:23 +0200

    Disable lazy auto update plugins

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -30,6 +30,4 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
 })
